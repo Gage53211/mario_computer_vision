8e26315b67058af06a7b450e7f7e3be4440f5a15
Gage53211 <gagemather@gmail.com>
2026-05-10 23:16:23 -0600

    minor changes

--- bin/detect.py
+++ bin/detect.py
@@ -1,5 +1,6 @@
 import cv2 as cv
 import numpy as np
+from matplotlib import pyplot as plt
 
 THREASHOLD = 0.8
 
@@ -8,7 +9,7 @@ def find_single_item (image, name):
     """Finds single item using name provided."""
 
     template = cv.cvtColor(cv.imread(f'templates/{name}.png'), cv.COLOR_BGR2GRAY)
-    res = cv.matchTemplate(image, template,cv.TM_CCOEFF_NORMED)
+    res = cv.matchTemplate(image, template, cv.TM_CCOEFF_NORMED)
     threshold = THREASHOLD
     coords = None 
     loc = np.where( res >= threshold)
@@ -23,7 +24,7 @@ def find_multiple_items (image, name):
     """Finds multiple items using name provided."""
 
     template = cv.cvtColor(cv.imread(f'templates/{name}.png'), cv.COLOR_BGR2GRAY)
-    res = cv.matchTemplate(image, template,cv.TM_CCOEFF_NORMED)
+    res = cv.matchTemplate(image, template, cv.TM_CCOEFF_NORMED)
     threshold = THREASHOLD
     loc = np.where( res >= threshold)
     coords = []
