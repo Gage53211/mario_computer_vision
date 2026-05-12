d7b886da6aac89f44a6bf7d9433ade23cf3c933c
Gage53211 <gagemather@gmail.com>
2026-05-11 23:57:38 -0600

    re-organized code into its own class and functions / general improvements

--- bin/play.py
+++ bin/play.py
@@ -7,111 +7,105 @@ import cv2 as cv
 import numpy as np
 import pygetwindow as gwindow
 from pynput.keyboard import Key, Controller
-from matplotlib import pyplot as plt
-
-def crop_top (image):
-    new_size = 0
-    try:
-        while image[new_size][100] != 157 and new_size < len(image):
-            new_size += 1
-        return new_size
-    except IndexError:
-        print("Could not crop top of screen, maybe the screen was black?")
-        return None
 
 
-def crop_left (image):
-    new_size = 0
-    try:
-        while image[100][new_size] != 157:
-            new_size += 1
-        return new_size
-    except IndexError:
-        print("Could not crop left of screen, maybe the screen was black?")
-        return None
+class Play ():
+    
+    is_finished = None
+    new_height = None
+    new_width = None
 
+    keyboard = None
+    clock = None
+    dec = None
 
-def crop_height (image):
-    new_size = image.shape[0] - 1
-    try:
-        while image[new_size][100] != 0 and image[new_size][100] != 90 and image[new_size][100] != 218:
-            new_size -= 1
-        return new_size
-    except IndexError:
-        print("Could not crop bottom of screen, maybe the screen was black?")
-        return None
+    def __init__ (self):
+        """This function preps the emulator for pynput input"""
 
+        pygame.init()
 
-def crop_width (image):
-    new_size = image.shape[1] - 1
-    try:
-        while image[100][new_size] != 157:
-            new_size -= 1
-        return new_size
-    except IndexError:
-            print("Could not crop right of screen, maybe the screen was black?")
-            return None
+        self.is_finished = False
+        self.new_height = None
+        self.new_width = None
+        self.size = None
+        self.window = None
 
+        self.keyboard = Controller()
+        self.clock = pygame.time.Clock()
+        self.dec = Decision()
+        
+        try:
+            self.window = gwindow.getWindowsWithTitle('Mesen - Super Mario Bros')[0]
+            print("Mesen Emulator Running Super Mario Found,", 
+                  "DO NOT MOVE WINDOW")
+        except IndexError:
+            print("Mesen Emulator Running Super Mario Not Found,", 
+                  "\nIf the emulator is running, Try renaming .nes file to \"Super Mario Bros\"")
+            exit()
 
-if __name__ == "__main__":
+        if self.window:
+            if not self.window.isMaximized: # brings window into view
+                self.window.restore()
+            self.window.activate() # focuses the window for pynput
+            time.sleep(.5) # wait for application to show up on screen
 
-    keyboard = Controller()
-    pygame.init()
+            # have the emulator display at 2x resolution
+            with self.keyboard.pressed(Key.alt):
+                self.keyboard.press('2')
+                time.sleep(.1)
+                self.keyboard.release('2')
+                time.sleep(.5)
 
-    try:
-        window = gwindow.getWindowsWithTitle('Mesen - Super Mario Bros. (World)')[0]
-    except IndexError:
-        print("Mesen Emulator Running Super Mario Not Found, Try renaming rom to \"Super Mario Bros. (World)\"")
-        exit()
-   
-    if window:
-        if not window.isMaximized: # brings window into view
-            window.restore()
-        window.activate() # focuses the window for pynput
-        time.sleep(.5) # wait for application to show up on screen
-        size = {
-            "top": window.top, "left": window.left, "width": window.width, "height": window.height
-        }
+            self.size = {
+                "top": self.window.top, "left": self.window.left, "width": self.window.width, "height": self.window.height
+            }
+            
+            # have the emulator reset the game
+            with self.keyboard.pressed(Key.ctrl):
+                self.keyboard.press('r')
+                time.sleep(.1)
+                self.keyboard.release('r')
+                time.sleep(1.5)
 
-        with keyboard.pressed(Key.ctrl):
-            keyboard.press('r')
+            # input start key
+            self.keyboard.press('w')
             time.sleep(.1)
-            keyboard.release('r')
-            time.sleep(1.5)
+            self.keyboard.release('w')
 
-        #input start key
-        keyboard.press('w')
-        time.sleep(.1)
-        keyboard.release('w')
-
-        #wait for black screen to clear then hold down right key
-        time.sleep(3.5)
-        keyboard.press(Key.right)
-
-        new_top = None
-        new_height = None
-        new_width = None
-
-        clock = pygame.time.Clock()
-        dec = Decision()
-        is_finished = False
+            # wait for black screen to clear then hold down right key
+            time.sleep(3.5)
+            self.keyboard.press(Key.right)
+    
+    def main_loop (self):
+        """
+        This function gets the position of Mario,
+        enemies, and obstacles from detect.py and
+        sends them to decision.py 25 times per 
+        second.
+        """
 
         with mss.MSS() as sct:
-            while not is_finished: # main loop for screen analysis
-                screenshot = np.array(sct.grab(size))
+            while not self.is_finished: 
+                screenshot = np.array(sct.grab(self.size))
                 screenshot_grey = cv.cvtColor(screenshot, cv.COLOR_BGRA2GRAY)
 
+                self.size = {
+                    "top": self.window.top, "left": self.window.left, "width": self.window.width, "height": self.window.height
+                }
+
                 # crop when conditions are correct
-                if new_top is None:
-                    new_top = crop_top (screenshot_grey)
-               
-                if new_height is None:
-                    new_height = crop_height (screenshot_grey)
+                if self.new_height is None:
+                    self.new_height = self.crop_height (screenshot_grey)
 
-                if new_width is None:    
-                    new_width = crop_width (screenshot_grey)
+                if self.new_width is None:    
+                    self.new_width = self.crop_width (screenshot_grey)
 
-                screenshot_grey = screenshot_grey[200 : new_height, 200 : new_width]
+                if self.new_height != -1 or self.new_width != -1:
+                    screenshot_grey = screenshot_grey[200 : self.new_height, 200 : self.new_width]
+                else:  
+                    print ("Could not crop screen... ",
+                           "\nThis could be due the screen being black. ",
+                           "\nIt could also be due to your display scale being set below or above 100%")
 
                 # detect all objects
                 mario_coords, mario_shape           = detect.find_single_item    (image=screenshot_grey, name="mario")
@@ -124,27 +118,46 @@ if __name__ == "__main__":
                 goombas, goomba_shape               = detect.find_multiple_items (image=screenshot_grey, name="goomba")
                 koopas, koopa_shape                 = detect.find_multiple_items (image=screenshot_grey, name="koopa")
 
-                is_finished = dec.make_decision(mario=mario_coords,
-                                                goombas=goombas,
-                                                pipes=pipes,
-                                                small_ground_gap=small_gap_coords,
-                                                ground_gap=gap_coords,
-                                                koopas=koopas,
-                                                blocks=blocks,
-                                                flag=flag_coords)
+                self.is_finished = self.dec.make_decision(mario=mario_coords,
+                                                          goombas=goombas,
+                                                          pipes=pipes,
+                                                          small_ground_gap=small_gap_coords,
+                                                          ground_gap=gap_coords,
+                                                          koopas=koopas,
+                                                          blocks=blocks,
+                                                          flag=flag_coords)
                 
-                clock.tick(25) # limits framerate to 25 frames per second.
+                self.clock.tick(25) # limits while loop to run 25 times per second.
 
         # jump onto the flagpole
-        keyboard.press('s')
+        self.keyboard.press('s')
         time.sleep(1)
-        keyboard.release('s')
+        self.keyboard.release('s')
         print ("1-1 Completed :)")
-               
 
+    def crop_height (self, image):
+        """Crops out anything thats not related to super mario on the bottom"""
 
-           
-   
+        new_size = image.shape[0] - 1
+        try:
+            while image[new_size][100] != 0 and image[new_size][100] != 90 and image[new_size][100] != 218:
+                new_size -= 1
+            return new_size
+        except IndexError:
+            return -1
 
+    def crop_width (self, image):
+        """Crops out anything thats not related to super mario on the right hand side"""
 
-       
+        new_size = image.shape[1] - 1
+        try:
+            while image[100][new_size] != 157:
+                new_size -= 1
+            return new_size
+        except IndexError:
+            return -1
+    
+if __name__ == "__main__":
+
+    game = Play()
+    game.main_loop()
